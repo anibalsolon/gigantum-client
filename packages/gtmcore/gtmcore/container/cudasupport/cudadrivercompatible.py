15de424ca6bf196ea9d8d14ec2d0a3d73b5dd298
Randal Burns <randburns@gmail.com>
2018-11-11 11:08:06 -0500

    Tests and error checking for CUDA support.
    MyPy and PEP8 too.

--- packages/gtmcore/gtmcore/container/cudasupport/cudadrivercompatible.py
+++ packages/gtmcore/gtmcore/container/cudasupport/cudadrivercompatible.py
@@ -29,11 +29,11 @@ class CudaDriverCompatible(object):
     # this table is a simplified version of https://github.com/NVIDIA/nvidia-docker/wiki/CUDA
     #  it should cover most reasonable configurations. 
     #  To interpret this data, cuda version 10.0 requires driver > 410.58
-    cuda_driver_lookup= { 10: {0: (410, 58)},
-                           9: {0: (384, 81),
-                               1: (387, 26),
-                               2: (396, 26)},
-                           8: {0: (375, 51)}} 
+    cuda_driver_lookup= {10: {0: (410, 58)},
+                         9: {0: (384, 81),
+                             1: (387, 26),
+                             2: (396, 26)},
+                         8: {0: (375, 51)}}
                                             
     @classmethod
     def check_version (cls, cuda_version: str ) -> bool : 
@@ -47,30 +47,28 @@ class CudaDriverCompatible(object):
                 True -> yes run CUDA 
                 False -> no CUDA not supported
         """
-        # get cuda verison major/minor
-        m = re.match("^([0-9]+)\.([0-9]+)$", str(cuda_version))
-        if m:
-            cuda_major = int(m.group(1))
-            cuda_minor = int(m.group(2))
-        else:
-            return False
+        try:
+            # get driver version major/minor
+            driver=os.environ['NVIDIA_DRIVER_VERSION']
+            m = re.match("^([0-9]+)\.([0-9]+)$", str(driver))
+            if m:
+                driver_major = int(m.group(1))
+                driver_minor = int(m.group(2))
 
-        # get driver version major/minor
-        driver=os.environ['NVIDIA_DRIVER_VERSION']
-        m = re.match("^([0-9]+)\.([0-9]+)$", str(driver))
-        if m:
-            driver_major = int(m.group(1))
-            driver_minor = int(m.group(2))
+                # get cuda verison major/minor
+                m = re.match("^([0-9]+)\.([0-9]+)$", str(cuda_version))
+                if m:
+                    cuda_major = int(m.group(1))
+                    cuda_minor = int(m.group(2))
 
-            # lookup table to check compatibility.
-            # if driver exceeds major and minor versions for this cuda, launch with cuda.
-            try:
-                mindriver = CudaDriverCompatible.cuda_driver_lookup[cuda_major][cuda_minor]
-                if driver_major > mindriver[0] or \
-                    (driver_major == mindriver[0] and driver_minor >= mindriver[1]):
-                    return True
-            except:
-                return False
+                    # lookup table to check compatibility.
+                    # if driver exceeds major and minor versions for this cuda, launch with cuda.
+                    mindriver = CudaDriverCompatible.cuda_driver_lookup[cuda_major][cuda_minor]
+                    if driver_major > mindriver[0] or \
+                        (driver_major == mindriver[0] and driver_minor >= mindriver[1]):
+                            return True
+        except KeyError as ke:
+            pass
 
-        return False                
-       
+        return False
+
